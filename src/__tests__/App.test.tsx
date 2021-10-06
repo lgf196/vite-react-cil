16331d20c24991ff1302d8053000d29aa555512e
lgf <18827634408@163.com>
2021-10-06 22:58:34 +0800

    edit test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,14 +2,16 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-// import '@testing-library/jest-dom';
+import { render, cleanup, getByTestId } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from '../App';
 
 afterEach(cleanup);
 
 describe('<App />', () => {
   it('renders without errors', () => {
-    render(<App />);
+    const { container } = render(<App />);
+    // a标签含有data-testid='aNoDisabled',进行检查
+    expect(getByTestId(container, 'aNoDisabled')).not.toBeDisabled();
   });
 });
